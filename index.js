ab810c8e67cec4588d8a2b6f218d84e86f922d60
Godana Emiru <godanaemiru@gmail.com>
2025-12-02 23:03:49 +0300

    String concatenation

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. String concatenation challenge
+
+const dealerName = "Maganjo Farms";
+const location = "Kampala";
+const product = "Maize";
+
+const message = "Dealer: " + dealerName + " | Location: " + location + " | Product: " + product;
+
+
+console.log(message);
